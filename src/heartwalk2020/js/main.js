896740a6c88104d45560baba95509bc897738aac
Colin Manuel <colin.manuel@gmail.com>
2019-07-08 10:40:05 -0500

    comment out js nav overrides

--- src/heartwalk2020/js/main.js
+++ src/heartwalk2020/js/main.js
@@ -47,28 +47,30 @@
       }
     });
 
-    $('#find').on('click', function(e){
-      e.preventDefault();
-      $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').toggle();
-    });
+    // $('#find').on('click', function(e){
+    //   e.preventDefault();
+    //   $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').toggle();
+    // });
 
-    $('.js--nav-about').on('click', function(e){
-      e.preventDefault();
-      $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').toggle();
-    });
+    // $('.js--nav-about').on('click', function(e){
+    //   e.preventDefault();
+    //   $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').toggle();
+    // });
 
-    $( "#find" ).focusin(function() {
-      $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').show();
-    });
+    // $( "#find" ).focusin(function() {
+    //   $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').show();
+    // });
 
-    $( ".js--nav-about" ).focusin(function() {
-      $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').hide();
-      $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').show();
-    });
+    // $( ".js--nav-about" ).focusin(function() {
+    //   $('.dropdown-menu-container.find, .dropdown-menu-container.find .dropdown-menu').hide();
+    //   $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').show();
+    // });
 
-    $( ".js--top-menu-contact" ).focusout(function() {
-      $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').hide();
-    });
+    // $( ".js--top-menu-contact" ).focusout(function() {
+    //   $('.dropdown-menu-container.about, .dropdown-menu-container.about .dropdown-menu').hide();
+    // });
+
+
 
     /******************/
     /* SEARCH SCRIPTS */
