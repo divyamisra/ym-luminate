f049944ce2fb42c61be97e0abfa1c41c20119337
Colin Manuel <colin.manuel@gmail.com>
2019-09-18 09:28:34 -0500

    swap event type 2 conditional

--- src/cyclenation/js/main.js
+++ src/cyclenation/js/main.js
@@ -1977,8 +1977,22 @@
         .prepend('<div class="order-2 order-sm-1 col-sm-4 col-md-3 col-8 offset-2 offset-sm-0"><a href="TRR/?pg=tfind&amp;fr_id=' + evID + '" class="button btn-secondary btn-block">Back</a></div>')
 
       // Add minimum validation to LOs team goal input
-      
-      if (eventType2 === 'Road') {
+      if (eventType2 === 'StationaryV2') {
+        $(loTeamGoal)
+        .val(goalPerBike)
+        .addClass('pl-0 border-left-0')
+        .wrap('<div class="input-group" />')
+        .before('<div class="input-group-prepend"><div class="input-group-text py-0 px-1 border-right-0 bg-white">$</div></div>')
+        .attr({
+          "min": goalPerBike,
+          "step": "100",
+          "aria-label": "Goal amount (to the nearest dollar)",
+          "data-parsley-validation-threshold": "1",
+          "data-parsley-trigger": "keyup",
+          "data-parsley-type": "number",
+          "data-parsley-min-message": minTeamGoalMsg
+        });
+      } else {
         var minTeamGoal = $(loTeamGoal).val();
         minTeamGoal = Number(minTeamGoal.replace(/[^0-9\.-]+/g, ""));
         
@@ -1989,21 +2003,6 @@
         .before('<div class="input-group-prepend"><div class="input-group-text py-0 px-1 border-right-0 bg-white">$</div></div>')
         .attr({
           "min": minTeamGoal,
-          "step": "100",
-          "aria-label": "Goal amount (to the nearest dollar)",
-          "data-parsley-validation-threshold": "1",
-          "data-parsley-trigger": "keyup",
-          "data-parsley-type": "number",
-          "data-parsley-min-message": minTeamGoalMsg
-        });
-      } else {
-        $(loTeamGoal)
-        .val(goalPerBike)
-        .addClass('pl-0 border-left-0')
-        .wrap('<div class="input-group" />')
-        .before('<div class="input-group-prepend"><div class="input-group-text py-0 px-1 border-right-0 bg-white">$</div></div>')
-        .attr({
-          "min": goalPerBike,
           "step": "100",
           "aria-label": "Goal amount (to the nearest dollar)",
           "data-parsley-validation-threshold": "1",
