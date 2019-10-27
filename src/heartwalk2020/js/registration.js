9c2398cf1727818bf22959e06de42fc47ca056a4
ironman67x <dean@dhwebworks.com>
2019-10-27 16:19:04 -0700

    remove offset on Next btn

--- src/heartwalk2020/js/registration.js
+++ src/heartwalk2020/js/registration.js
@@ -827,8 +827,8 @@
                 $('.list-component-paginator').hide();
             }
 
-            $('#friend_potion_next')
-                .wrap('<div class="order-1 order-sm-2 col-sm-4 offset-md-6 col-md-3 col-8 offset-2 mb-3"/>');
+            //$('#friend_potion_next')
+            //    .wrap('<div class="order-1 order-sm-2 col-sm-4 offset-md-6 col-md-3 col-8 offset-2 mb-3"/>');
 
             //$('#team_find_section_footer')
             //    .prepend('<div class="order-2 order-sm-1 col-sm-4 col-md-3 col-8 offset-2 offset-sm-0"><a href="TRR/?pg=tfind&amp;fr_id=' + evID + '" class="button btn-secondary btn-block">Back</a></div>')
