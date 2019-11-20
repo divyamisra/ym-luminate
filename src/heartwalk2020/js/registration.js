538a837c2cc2f91c98fb468964ca0ecc06ebd4be
ironman67x <dean@dhwebworks.com>
2019-11-19 18:48:02 -0800

    wrap additional ptype amt

--- src/heartwalk2020/js/registration.js
+++ src/heartwalk2020/js/registration.js
@@ -1060,6 +1060,9 @@
             }
             $('#part_type_additional_gift_container .otherAmt span').html("$");
 
+	    //wrap additional amount radio and label in own div for acc
+	    $('.input-container label:contains(Other Amount)').prev('input').andSelf().wrapAll('<div class="donation-level-row-decoration-container"></div>');
+		
             //connect of for on label to missing id on field
             $('input.goal').attr('id',$('input.goal').closest('.part-type-decoration-messages').find('label').attr("for"));
 
