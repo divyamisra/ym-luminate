f7a8269eaf550944ebbc01fec1b753ffab6216d0
ironman67x <dean@dhwebworks.com>
2020-06-17 19:16:59 -0700

    update hint text

--- src/fieldday/js/registration.js
+++ src/fieldday/js/registration.js
@@ -765,7 +765,7 @@
                     $('#team_find_new_team_attributes').addClass('no-companies');
                     $('#team_find_new_team_name, #team_find_new_fundraising_goal').addClass('col-md-6');
                 } else {
-                    $('#team_find_new_company_selection_container').append("<span class='hint-text hidden-xs'>You can't skip this step! If you don't see your company in the list, please <a href='#'>contact your local staff to get your company set up today!</a></span>");
+                    $('#team_find_new_company_selection_container').append("<span class='hint-text hidden-xs'>You can't skip this step! If you don't see your company in the list, please <a href='#'>contact your local staff</a> to get your company set up today!</span>");
                 }
 		$('#team_find_new_team_attributes').before($('#team_find_new_team_company'));
             } else if (regType === 'joinTeam') {
@@ -1124,7 +1124,7 @@
                 $(joinTeamLink).attr('href', updatedJoinTeamLink);
             }
 
-            //$('#team_find_new_fundraising_goal_input_hint').text('You can increase your team\'s goal, but the amount shown below is your team\'s suggested fundraising minimum.');
+            $('#team_find_new_fundraising_goal_input_hint').hide(); //text('You can increase your team\'s goal, but the amount shown below is your team\'s suggested fundraising minimum.');
             $('#previous_step span').text('Back');
         }
         // END TFIND
