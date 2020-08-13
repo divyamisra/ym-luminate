5fb6e2dcb41d99c664390ed36accea80f87c75c2
Cheryl Herrick <cherylherrick@users.noreply.github.com>
2020-08-13 14:14:12 -0400

    field title a11y

--- src/fieldday/js/registration.js
+++ src/fieldday/js/registration.js
@@ -1318,9 +1318,6 @@
 		    }
 	    });
 
-      if ($('input').attr('id') === 'fr_team_name') {
-        var label = $(this).closest('.form-content').find('label').html("Team Name is required");
-      }
 
       /*Removing Mobile Opt-in Option*/
 
@@ -2155,6 +2152,12 @@
             if ($('body').is('.pg_regsummary')) {
               $('.reg-summary-address-info .reg-summary-edit-link a').attr('aria-label', 'Edit your registration information');
               $('.js--edit-ptype').attr('aria-label', 'Edit your registration information');
+            }
+
+
+
+            if ($('body').is('.pg_tfind')) {
+              $('#fr_team_name').attr('title', 'Team Name required');
             }
 
 
