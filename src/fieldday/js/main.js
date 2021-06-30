2875193e8088a3b82e68afe87fa175354f73f71e
tidalwade <ryantwade@hotmail.com>
2021-06-30 11:35:06 -0400

    team accessibility edits

--- src/fieldday/js/main.js
+++ src/fieldday/js/main.js
@@ -293,7 +293,7 @@
 
                 if ( eventMapLink.indexOf("http://") == 0 || eventMapLink.indexOf("https://") == 0 || eventMapLink.indexOf("www") == 0)  {
 
-                  var companyMap = '<a target="_blank" aria-title="Google map for '+ company.companyname +' location" href="' + eventMapLink + '">' + '<p>' + company.eventlocationname + '</p> <p>' + company.eventcity + ', ' + company.eventstate + '</p>' + '</a>';
+                  var companyMap = '<a target="_blank" aria-label="Google map for '+ company.companyname +' location" href="' + eventMapLink + '">' + '<p>' + company.eventlocationname + '</p> <p>' + company.eventcity + ', ' + company.eventstate + '</p>' + '</a>';
 
                   $('.js--company-link').html(companyMap)
 
@@ -308,7 +308,7 @@
 
                   var eventMapLink = 'https://www.google.com/maps/place/' + companyAddress;
 
-                  var companyMap = '<a target="_blank" href="' + eventMapLink + '">' + '<p>' + company.eventlocationname + '</p> <p>' + company.eventcity + ', ' + company.eventstate + '</p>' + '</a>';
+                  var companyMap = '<a target="_blank" aria-label="Google map for '+ company.companyname +' href="' + eventMapLink + '">' + '<p>' + company.eventlocationname + '</p> <p>' + company.eventcity + ', ' + company.eventstate + '</p>' + '</a>';
 
                   // $("'"+companyMap+"'").appendTo('.js--company-link')
 
