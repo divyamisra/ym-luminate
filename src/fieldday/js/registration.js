b313fe8d751d75e99e3b714ac1cb5d756a61ba27
tidalwade <ryantwade@hotmail.com>
2021-07-06 12:24:14 -0400

    remove tabindex 2nd try

--- src/fieldday/js/registration.js
+++ src/fieldday/js/registration.js
@@ -1598,7 +1598,7 @@
         $('.donation-level-row-container.enterAmt label.donation-level-row-label').text('Other Amount');
 	$('.donation-level-row-label-no-gift').parent().addClass('notTime');
   $('.enterAmt .input-container > span').next('input').andSelf().wrapAll("<div class='enterAmt-other hidden'></div>");
-  $('.donation-level-row-container').attr('tabindex', '0');
+  // $('.donation-level-row-container').attr('tabindex', '0');
 
         $(".donation-level-amount-text").each(function() {
             $(this).text($(this).text().replace(".00", ""));
