b5e510a4cbb6de8ed3ede470191d510af17e24ab
tidalwade <ryantwade@hotmail.com>
2021-09-21 11:52:33 -0400

    additional WOI setup

--- src/women-of-impact/js/main.js
+++ src/women-of-impact/js/main.js
@@ -126,7 +126,7 @@
             document.getElementById('contentStart').scrollIntoView();
         });
 
-        if ($('body').is('.pg_Stem_HQ')) {
+        if ($('body').is('.pg_WOI_HQ')) {
             $('.js__skip-to').on('click', function (e) {
                 e.preventDefault();
                 $('html, body').animate({
@@ -611,10 +611,10 @@
         $('.js--header-company-search').on('submit', function (e) {
             e.preventDefault();
             var companySearched = encodeURIComponent($('#companySearch').val());
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=company&cross_event=true&company=' + companySearched;
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=company&cross_event=true&company=' + companySearched;
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=company'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&company=' + companySearched;
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=company'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&company=' + companySearched;
             }
         });
 
@@ -622,10 +622,10 @@
         $('.js--header-zip-search').on('submit', function (e) {
             e.preventDefault();
             var zipSearched = encodeURIComponent($('#zipSearch').val());
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=zip&cross_event=true&zip=' + zipSearched;
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=zip&cross_event=true&zip=' + zipSearched;
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=zip'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&zip=' + zipSearched;
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=zip'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&zip=' + zipSearched;
             }
         });
 
@@ -633,20 +633,20 @@
         $('.js--header-state-search').on('submit', function (e) {
             e.preventDefault();
             var stateSearch = encodeURIComponent($('#stateSearch').val());
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=state&cross_event=true&state=' + stateSearched;
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=state&cross_event=true&state=' + stateSearched;
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=state'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&state=' + stateSearched;
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=state'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&state=' + stateSearched;
             }
         });
 
 
         $('#stateSearch').on('change', function () {
             var stateSearch = encodeURIComponent($('#stateSearch').val());
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=state&cross_event=true&state=' + stateSearch;
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=state&cross_event=true&state=' + stateSearch;
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=state'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&state=' + stateSearch;
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=state'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&state=' + stateSearch;
             }
         });
 
@@ -656,10 +656,10 @@
             var firstName = encodeURIComponent($('#participantSearchFirst').val());
             var lastName = encodeURIComponent($('#participantSearchLast').val());
 
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=participant&cross_event=true' + (firstName ? '&first_name=' + firstName : '') + (lastName ? '&last_name=' + lastName : '');
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=participant&cross_event=true' + (firstName ? '&first_name=' + firstName : '') + (lastName ? '&last_name=' + lastName : '');
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=participant'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + (firstName ? '&first_name=' + firstName : '') + (lastName ? '&last_name=' + lastName : '');
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=participant'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + (firstName ? '&first_name=' + firstName : '') + (lastName ? '&last_name=' + lastName : '');
             }
         });
 
@@ -669,10 +669,10 @@
             var teamName = encodeURIComponent($('#teamSearch').val());
             cd.getTeams(teamName, (isCrossEventSearch === "true" ? true : false));
 
-            if ( $('body').is('.pg_Stem_General') || $('body').is('.pg_Stem_Landing_Page') ) {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=team&cross_event=true&team_name=' + teamName;
+            if ( $('body').is('.pg_Woi_General') || $('body').is('.pg_Woi_Landing_Page') ) {
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=team&cross_event=true&team_name=' + teamName;
             } else {
-              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=team'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&team_name=' + teamName;
+              window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=team'  + (evID ? '&cross_event=false&fr_id=' + evID : '&cross_event=true') + '&team_name=' + teamName;
             }
         });
 
@@ -1556,7 +1556,7 @@
                 console.log('teammate search');
                 var firstName = encodeURIComponent($('#greeting-search-first-name').val());
                 var lastName = encodeURIComponent($('#greeting-search-last-name').val());
-                window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=participant&cross_event=false&fr_id=' + evID + (firstName ? '&first_name=' + firstName : '') +
+                window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=participant&cross_event=false&fr_id=' + evID + (firstName ? '&first_name=' + firstName : '') +
                     (lastName ? '&last_name=' + lastName : '');
             });
 
@@ -1564,7 +1564,7 @@
             $('.js--greeting-team-search-form').on('submit', function (e) {
                 e.preventDefault();
                 var teamName = encodeURIComponent($('#greeting-search-team').val());
-                window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=Stem_Search&search_type=team&cross_event=false&fr_id=' + evID + '&team_name=' + teamName;
+                window.location.href = luminateExtend.global.path.secure + 'SPageServer/?pagename=WOI_Search&search_type=team&cross_event=false&fr_id=' + evID + '&team_name=' + teamName;
             });
 
             //Adding aria labels for additional nav
@@ -1743,10 +1743,10 @@
                                         personalVideoEmbedUrl = 'https://www.youtube.com/embed/' + videoId + '?wmode=opaque&amp;rel=0&amp;showinfo=0';
                                     }
                                 }
-                                videoEmbedHtml = '<iframe cc_load_policy=1 class="embed-responsive-item" src="' + personalVideoEmbedUrl + '" title="American Heart Association STEM Video" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+                                videoEmbedHtml = '<iframe cc_load_policy=1 class="embed-responsive-item" src="' + personalVideoEmbedUrl + '" title="American Heart Association Women of Impact Video" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
                             } else {
                                 // TODO - show default video
-                                videoEmbedHtml = '<iframe cc_load_policy=1 width="560" height="315" src="https://www.youtube.com/embed/LryhjU1bEC4" title="About American Heart Association STEM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+                                videoEmbedHtml = '<iframe cc_load_policy=1 width="560" height="315" src="https://www.youtube.com/embed/LryhjU1bEC4" title="About American Heart Association Women of Impact" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
                             }
                             $('.js--personal-video-container').append(videoEmbedHtml);
                         },
@@ -1863,7 +1863,7 @@
             // Populate company name from page title
             var pageTitle = jQuery('head title').text().trim();
             var start_pos = pageTitle.indexOf(':') + 1;
-            var end_pos = pageTitle.indexOf('- STEM', start_pos);
+            var end_pos = pageTitle.indexOf('- Women of Impact', start_pos);
             var currentCompanyName = pageTitle.substring(start_pos, end_pos).trim();
             var currentCompanyId = getURLParameter(currentUrl, 'company_id');
 
@@ -2273,7 +2273,7 @@
         }
 
 
-        if ($('body').is('.pg_Stem_Search')) {
+        if ($('body').is('.pg_Woi_Search')) {
             // Leader_For_Life Search Page
             var clearSearchResults = function () {
                 $('.js--event-results-container, .alert').attr('hidden', true);
@@ -2413,7 +2413,7 @@
 
         //Landong Page
 
-        if($('body').is('.pg_Stem_Landing_Page')) {
+        if($('body').is('.pg_Woi_Landing_Page')) {
           //Search functionality
 
           $('#company-page-search, #participant-page-search').on('click', function(){
