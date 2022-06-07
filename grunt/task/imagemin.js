6f0b6d3c8a23328f397ad329c9154af0463eb97d
ironman67x <dean@dhwebworks.com>
2022-06-07 16:09:53 -0700

    add ahc prizes2023

--- grunt/task/imagemin.js
+++ grunt/task/imagemin.js
@@ -72,6 +72,7 @@ module.exports = {
         cwd: 'src/middle-school/image/',
         src: [
           '*.{gif,GIF,jpg,JPG,png,PNG,svg,SVG}',
+          'prizes2023/*.{gif,GIF,jpg,JPG,png,PNG,svg,SVG}',
           'fy22/*.{gif,GIF,jpg,JPG,png,PNG,svg,SVG}',
           'fy23/*.{gif,GIF,jpg,JPG,png,PNG,svg,SVG}'
         ],
