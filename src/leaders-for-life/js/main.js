a3ae8a5905389515e76c1572bfbeb5c33c0a2b08
Xidorian <Xidorian@gmail.com>
2022-07-08 20:00:16 -0700

    coding

--- src/leaders-for-life/js/main.js
+++ src/leaders-for-life/js/main.js
@@ -1272,8 +1272,10 @@
                                             var teamImage = typeof teamImages[0].customUrl === 'string' && teamImages[0].customUrl.length ? teamImages[0].customUrl : teamImages[0].originalUrl;
                                             if (teamImage === teamImages[0].originalUrl) {
                                               var isDefaultImage = ' style="clip-path: none;"';
+                                            } else {
+                                              isDefaultImage = ''
                                             }
-                                            var topTeamRow = `<div class="col-sm-6 col-md-4 pt-4 px-md-3"><a href="TR/?team_id=${teamId}&amp;pg=team&amp;fr_id=${eventId}"><div class="bg-red-deep"><div><img src="${teamImage}" alt="Photo of ${teamName}"></div></div><div class="align-items-center bg-red-deep d-flex justify-content-center text-center"><p class="p-2 text-white"><strong>${teamName}</strong></p></div></a></div>`;
+                                            var topTeamRow = `<div class="col-sm-6 col-md-4 pt-4 px-md-3"><a href="TR/?team_id=${teamId}&amp;pg=team&amp;fr_id=${eventId}"><div class="bg-red-deep"><div${isDefaultImage}><img src="${teamImage}" alt="Photo of ${teamName}"></div></div><div class="align-items-center bg-red-deep d-flex justify-content-center text-center"><p class="p-2 text-white"><strong>${teamName}</strong></p></div></a></div>`;
 
                                             deferred.resolve(topTeamRow);
                                         },
