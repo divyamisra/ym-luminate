22aca90dddc29e6055cc41c32d092eb20b1af61e
Xidorian <Xidorian@gmail.com>
2022-08-10 10:10:45 -0700

    Nominee styles

--- src/women-of-impact/js/main.js
+++ src/women-of-impact/js/main.js
@@ -1278,12 +1278,12 @@
                                             var teamImages = luminateExtend.utils.ensureArray(response.getTeamPhotoResponse.photoItem);
                                             teamImages[0].originalUrl = '../images/content/pagebuilder/WOI-Default-Photo.png';
                                             var teamImage = typeof teamImages[0].customUrl === 'string' && teamImages[0].customUrl.length ? teamImages[0].customUrl : teamImages[0].originalUrl;
-                                            if (teamImage === teamImages[0].originalUrl) {
-                                              var isDefaultImage = ' style="clip-path: none;"';
-                                            } else {
-                                              isDefaultImage = ''
-                                            }
-                                            var topTeamRow = `<div class="col-sm-6 col-md-4 pt-4 px-md-3"><a href="TR/?team_id=${teamId}&amp;pg=team&amp;fr_id=${eventId}"><div class="bg-red-deep"><div${isDefaultImage}><img src="${teamImage}" alt="Photo of ${teamName}"></div></div><div class="align-items-center bg-red-deep d-flex justify-content-center text-center"><p class="p-2 text-white"><strong>${teamName}</strong></p></div></a></div>`;
+                                            // if (teamImage === teamImages[0].originalUrl) {
+                                            //   var isDefaultImage = ' style="clip-path: none;"';
+                                            // } else {
+                                            //   isDefaultImage = ''
+                                            // }
+                                            var topTeamRow = `<div class="col-sm-6 col-md-4 pt-4 px-md-3"><a href="TR/?team_id=${teamId}&amp;pg=team&amp;fr_id=${eventId}"><div class="bg-white"><div><img src="${teamImage}" alt="Photo of ${teamName}"></div></div><div class="align-items-center bg-white d-flex justify-content-center text-center"><p class="p-2 text-body"><strong>${teamName}</strong></p></div></a></div>`;
 
                                             deferred.resolve(topTeamRow);
                                         },
